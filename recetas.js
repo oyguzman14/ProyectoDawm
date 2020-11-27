f2eeb2c1009f84de6854fbd1d3250bcdf8f43b87
Luis Anchundia <leanchun@espol.edu.ec>
2020-11-26 23:06:34 -0500

    creacion de buscador

--- recetas.js
+++ recetas.js
@@ -17,3 +17,19 @@ function filtrarRecetas(){
     }
 }
 
+function filtrarCatalogo(){
+    let fl=document.getElementById("myInput");
+    let filtro=fl.value.toUpperCase();
+    let filas=document.getElementsByClassName("row")[1]
+    let tarjetas=filas.getElementsByClassName("bocado")
+    for (let elemento of tarjetas){
+        let comparar=elemento.textContent.toUpperCase();
+        if(comparar.indexOf(filtro)>-1){
+            elemento.parentNode.parentNode.parentNode.style.display= "";
+        }else{
+            elemento.parentNode.parentNode.parentNode.style.display="none";
+        }
+    }
+
+
+}
