f30cadc9a1da5f7b55beeafe439732779d27569d
Luis Anchundia <leanchun@espol.edu.ec>
2020-12-03 22:41:44 -0500

    mapa arreglado

--- js/mapa.js
+++ js/mapa.js
@@ -1,13 +1,8 @@
- "use strict";
+let map;
 
-      let map;
-
-      function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-          center: {
-            lat: -34.397,
-            lng: 150.644
-          },
-          zoom: 8
-        });
-      }
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -2.133807, lng: -79.897713 },
+    zoom: 8,
+  });
+}
